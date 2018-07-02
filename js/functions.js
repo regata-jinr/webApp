a30a0e551d8fdaf5716a737e681330be4813cfdd
bdrum <boris.rumyantsev@cern.ch>
2018-07-02 15:59:05 +0300

    Added new pages:   equipments.html   staff.html Bugs fixed, perfomance improved

--- js/functions.js
+++ js/functions.js
@@ -1,13 +1,34 @@
-function Show(id) {
-  document.getElementById(id).className = 'show';
+var elList = document.getElementsByClassName("barPoint");
+//TODO: refactor this block into one function ▼
+for (var i = 0; i < elList.length; i++) {
+  (function() {
+    var str = elList.item(i).id;
+    document.getElementById(str).addEventListener("mouseover", function() {
+      document.getElementById(str.replace('Main', '')).className = 'show'
+    }, false);
+    document.getElementById(str).addEventListener("mouseout", function() {
+      document.getElementById(str.replace('Main', '')).className = 'hide'
+    }, false);
+  }());
 }
 
-function Hide(id) {
-  document.getElementById(id).className = 'hide';
+var blockList = ['naaList', 'equipmentList', 'projectsList', 'staffList', 'clientsList'];
+
+for (var i = 0; i < blockList.length; i++) {
+  (function() {
+    var str = blockList[i];
+    document.getElementById(str).addEventListener("mouseover", function() {
+      document.getElementById(str).className = 'show'
+    }, false);
+    document.getElementById(str).addEventListener("mouseout", function() {
+      document.getElementById(str).className = 'hide'
+    }, false);
+  }());
 }
+//TODO: refactor this block into one function ▲
 
 function setFont(arr, fs) {
-  if (arr[0]==null) return;
+  if (arr[0] == null) return;
   [].forEach.call(arr, function(el) {
     el.style.fontSize = fs + 'px';
   });
@@ -26,16 +47,17 @@ function FontByScreen(width) {
   setFont(header, fs);
 }
 go();
+
 window.addEventListener('resize', go);
 
 function go() {
-FontByScreen(document.documentElement.clientWidth);
+  FontByScreen(document.documentElement.clientWidth);
 }
 
 function createNaaTabContent() {
   Array.from(document.getElementsByClassName("activeManButton")).forEach(function(item) {
-   item.className = "manButtons";
-});
+    item.className = "manButtons";
+  });
   document.getElementById('naaButton').className = "activeManButton";
 
   var wind = document.getElementsByClassName("demoWindow")[0];
@@ -53,8 +75,8 @@ function createNaaTabContent() {
 
 function createProcTabContent() {
   Array.from(document.getElementsByClassName("activeManButton")).forEach(function(item) {
-   item.className = "manButtons";
-});
+    item.className = "manButtons";
+  });
   document.getElementById('procButton').className = "activeManButton";
   var wind = document.getElementsByClassName("demoWindow")[0];
   wind.innerHTML = '';
@@ -65,14 +87,14 @@ function createProcTabContent() {
   wind.appendChild(par);
   var imported = document.createElement("IMG");
   imported.id = "procImg";
-  imported.src="imgs/samples.jpeg";
-    wind.appendChild(imported);
+  imported.src = "imgs/samples.jpeg";
+  wind.appendChild(imported);
 
 }
 
 function createCommTabContent() {
   Array.from(document.getElementsByClassName("activeManButton")).forEach(function(item) {
-   item.className = "manButtons";
-});
+    item.className = "manButtons";
+  });
   document.getElementById('commButton').className = "activeManButton";
 }
--- js/spectre.js
+++ js/spectre.js
@@ -1,13 +1,13 @@
-function addAxes (svg, xAxis, yAxis, margin, chartWidth, chartHeight) {
+function addAxes(svg, xAxis, yAxis, margin, chartWidth, chartHeight) {
   svg.append('clipPath')
     .attr('id', 'axes-clip')
     .append('polygon')
-      .attr('points', (-margin.left) + ',' + (-margin.top)  + ' ' +
-                      (chartWidth - 1) + ',' + (-margin.top)                 + ' ' +
-                      (chartWidth - 1) + ', ' +
-                      (chartWidth + margin.right)    + ', ' +
-                      (chartWidth + margin.right)    + ',' + (chartHeight + margin.bottom) + ' ' +
-                      (-margin.left)                 + ',' + (chartHeight + margin.bottom));
+    .attr('points', (-margin.left) + ',' + (-margin.top) + ' ' +
+      (chartWidth - 1) + ',' + (-margin.top) + ' ' +
+      (chartWidth - 1) + ', ' +
+      (chartWidth + margin.right) + ', ' +
+      (chartWidth + margin.right) + ',' + (chartHeight + margin.bottom) + ' ' +
+      (-margin.left) + ',' + (chartHeight + margin.bottom));
 
   var axes = svg.append('g')
     .attr('clip-path', 'url(#axes-clip)');
@@ -17,26 +17,30 @@ function addAxes (svg, xAxis, yAxis, margin, chartWidth, chartHeight) {
     .attr('transform', 'translate(0,' + chartHeight + ')')
     .call(xAxis)
     .append('text')
-      .attr("text-anchor", "end")
-      .attr("x", chartWidth)
-      .attr("y", -5)
-      .text('Energy (kEv)');
+    .attr("text-anchor", "end")
+    .attr("x", chartWidth)
+    .attr("y", -5)
+    .text('Energy (kEv)');
   axes.append('g')
     .attr('class', 'y axis')
     .call(yAxis)
     .append('text')
-      .attr('transform', 'rotate(-90)')
-      .attr('y', 6)
-      .attr('dy', '.71em')
-      .style('text-anchor', 'end')
+    .attr('transform', 'rotate(-90)')
+    .attr('y', 6)
+    .attr('dy', '.71em')
+    .style('text-anchor', 'end')
 }
 
-function drawPaths (svg, data, x, y) {
+function drawPaths(svg, data, x, y) {
 
   var medianLine = d3.svg.line()
     .interpolate('basis')
-    .x(function (d) { return x(d.energy); })
-    .y(function (d) { return y(d.counts); });
+    .x(function(d) {
+      return x(d.energy);
+    })
+    .y(function(d) {
+      return y(d.counts);
+    });
 
   svg.datum(data);
 
@@ -46,10 +50,10 @@ function drawPaths (svg, data, x, y) {
     .attr('clip-path', 'url(#rect-clip)');
 }
 
-function addMarker (marker, svg, chartHeight, x) {
-  var xPos =  x(marker.energy),
-      yPosStart =  0,
-      yPosEnd =  chartHeight;
+function addMarker(marker, svg, chartHeight, x) {
+  var xPos = x(marker.energy),
+    yPosStart = 0,
+    yPosEnd = chartHeight;
   var markerG = svg.append('g')
     .attr('class', 'marker ' + marker.type.toLowerCase())
     .attr('transform', 'translate(' + xPos + ', ' + yPosEnd + ')')
@@ -60,98 +64,99 @@ function addMarker (marker, svg, chartHeight, x) {
     .attr('transform', 'translate(' + xPos + ', ' + yPosStart + ')')
     .attr('opacity', 1);
   markerG.append('path')
-    .attr('d', 'M' + xPos + ',' + yPosEnd)
+    .attr('d', 'M' + xPos + ',' + (yPosEnd))
     .attr('stroke', 'red')
     .transition()
-      .duration(1000)
-      .attr('d', 'M' + xPos + ',' + (yPosStart+50) + 'l'+ 0 + ',' + (yPosEnd-50))
-      .attr('stroke', 'red');
+    .duration(1000)
+    .attr('d', 'M' + xPos + ',' + (yPosStart + 60) + 'l' + 0 + ',' + (yPosEnd - 60))
+    .attr('stroke', 'red');
 
   markerG.append('text')
     .attr('x', xPos)
-    .attr('y', xPos*0.9)
+    .attr('y', xPos * 0.9)
     .style('fill', 'red')
     .style("font-size", "20px")
     .text(marker.type);
+
 }
 
-function startTransitions (svg, chartWidth, chartHeight, rectClip, markers, x) {
+function startTransitions(svg, chartWidth, chartHeight, rectClip, markers, x) {
   rectClip.transition()
-    .duration(1000*markers.length)
+    .duration(1000 * markers.length)
     .attr('width', chartWidth);
 
-  markers.forEach(function (marker, i) {
-    setTimeout(function () {
+  markers.forEach(function(marker, i) {
+    setTimeout(function() {
       addMarker(marker, svg, chartHeight, x);
-    }, 1000 + 500*i);
+    }, 1000 + 500 * i);
   });
 }
 
-function makeChart (data, markers) {
-  var svgWidth  = document.getElementsByClassName("demoWindow")[0].offsetWidth,
-      svgHeight = document.getElementsByClassName("demoWindow")[0].offsetHeight - document.getElementById('graphLegend').offsetHeight,
-      margin = { top: 30, right: 20, bottom: 30, left: 40 },
-      chartWidth  = svgWidth  - margin.left - margin.right,
-      chartHeight = svgHeight - margin.top  - margin.bottom;
+function makeChart(data, markers) {
+  var svgWidth = document.getElementsByClassName("demoWindow")[0].offsetWidth,
+    svgHeight = document.getElementsByClassName("demoWindow")[0].offsetHeight - document.getElementById('graphLegend').offsetHeight,
+    margin = {
+      top: 30,
+      right: 20,
+      bottom: 30,
+      left: 40
+    },
+    chartWidth = svgWidth - margin.left - margin.right,
+    chartHeight = svgHeight - margin.top - margin.bottom;
 
   var x = d3.scale.linear().range([0, chartWidth])
-            .domain(d3.extent(data, function (d) { return d.energy; })),
-      y = d3.scale.linear().range([chartHeight, 0])
-            .domain([0, d3.max(data, function (d) { return d.bckgrCountsUp; })]);
+    .domain(d3.extent(data, function(d) {
+      return d.energy;
+    })),
+    y = d3.scale.linear().range([chartHeight, 0])
+    .domain([0, d3.max(data, function(d) {
+      return d.bckgrCountsUp;
+    })]);
 
   var xAxis = d3.svg.axis().scale(x).orient('bottom')
-                .innerTickSize(-chartHeight).outerTickSize(0).tickPadding(10),
-      yAxis = d3.svg.axis().scale(y).orient('left')
-                .innerTickSize(-chartWidth).outerTickSize(0).tickPadding(10);
+    .innerTickSize(-chartHeight).outerTickSize(0).tickPadding(10),
+    yAxis = d3.svg.axis().scale(y).orient('left')
+    .innerTickSize(-chartWidth).outerTickSize(0).tickPadding(10);
 
   var svg = d3.select('.demoWindow').append('svg')
-    .attr('width',  svgWidth)
+    .attr('width', svgWidth)
     .attr('height', svgHeight)
     .append('g')
-      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
   var rectClip = svg.append('clipPath')
     .attr('id', 'rect-clip')
     .append('rect')
-      .attr('width', 0)
-      .attr('height', chartHeight);
-
-      // svg.append("text")
-      //         .attr("x", (-margin.left+5))
-      //         .attr("y", 0 - (margin.top-16))
-      //         .attr("text-anchor", "right")
-      //         .attr("text-rendering", "geometricPrecision")
-      //         .attr("text-align", "justify")
-      //         .style("font-size", "16px")
-      //         .text("Нейтронный активационный анализ (НАА) − ядерно-физический метод определения состава вещества, основанный на активации атомных ядер с помощью нейтронов и исследовании радиоактивного излучения, возникающего вследствие возбуждения атомных ядер.");
+    .attr('width', 0)
+    .attr('height', chartHeight);
 
   addAxes(svg, xAxis, yAxis, margin, chartWidth, chartHeight);
   drawPaths(svg, data, x, y);
   startTransitions(svg, chartWidth, chartHeight, rectClip, markers, x);
 }
 
-d3.json('data/spec.json', function (error, rawData) {
+d3.json('data/specUniq.json', function(error, rawData) {
   if (error) {
     console.error(error);
     return;
   }
 
-  var data = rawData.map(function (d) {
+  var data = rawData.map(function(d) {
     return {
-      energy:  d.energy,
+      energy: d.energy,
       bckgrCountsLow: d.bckgrCountsLow / 1000,
       counts: d.counts / 1000,
       bckgrCountsUp: d.bckgrCountsUp / 1000,
     };
   });
 
-  d3.json('data/markers.json', function (error, markerData) {
+  d3.json('data/markers.json', function(error, markerData) {
     if (error) {
       console.error(error);
       return;
     }
 
-    var markers = markerData.map(function (marker) {
+    var markers = markerData.map(function(marker) {
       return {
         energy: marker.energy,
         type: marker.type,
